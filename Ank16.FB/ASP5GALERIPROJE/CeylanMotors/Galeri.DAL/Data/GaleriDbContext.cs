674e2a7ab2cbb5d6c9775e2dd75c06ecc07231d3
furkanbaybura <furkan.baybura@bilgeadamboost.com>
2024-05-21 09:15:05 +0300

    Son çalışmalar

---
+++ Ank16.FB/ASP5GALERIPROJE/CeylanMotors/Galeri.DAL/Data/GaleriDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Galeri.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galeri.DAL.Data
+{
+    public class GaleriDbContext : DbContext
+    {
+        public DbSet<Marka> Markalar {  get; set; }
+        public DbSet<Mode> Modeller { get; set; }
+        public GaleriDbContext(DbContextOptions options) : base(options) { }
+    }
+}
---
+++ Ank16.FB/ASP5GALERIPROJE/CeylanMotors/Galeri.DAL/Repositories/Abstract/IRepo.cs
@@ -0,0 +1,14 @@
+﻿using Galeri.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galeri.DAL.Repositories.Abstract
+{
+    public interface IRepo<TEntity> where TEntity : BaseEntity
+    {
+        
+    }
+}
