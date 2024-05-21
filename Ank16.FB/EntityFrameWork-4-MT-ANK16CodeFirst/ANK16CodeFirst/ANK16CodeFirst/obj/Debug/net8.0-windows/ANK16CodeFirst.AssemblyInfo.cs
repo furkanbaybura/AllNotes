9775b066cd69d75b8911ee852f1100ce13e1c842
furkanbaybura <furkan.baybura@bilgeadamboost.com>
2024-05-21 09:53:49 +0300

    +

--- Ank16.FB/EntityFrameWork-4-MT-ANK16CodeFirst/ANK16CodeFirst/ANK16CodeFirst/obj/Debug/net8.0-windows/ANK16CodeFirst.AssemblyInfo.cs
+++ Ank16.FB/EntityFrameWork-4-MT-ANK16CodeFirst/ANK16CodeFirst/ANK16CodeFirst/obj/Debug/net8.0-windows/ANK16CodeFirst.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ANK16CodeFirst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc62224b81b20a2c9dd53ab2b483e81956f7946d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ANK16CodeFirst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ANK16CodeFirst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
